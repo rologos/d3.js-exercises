5ad94800d4c74ee362bdecc324bb030d791f1494
rologos <170283129+rologos@users.noreply.github.com>
2024-10-25 17:42:17 -0500

    new excercise, read file to console

---
+++ ch03/js/main.js
@@ -0,0 +1,8 @@
+const svg = d3.select(".responsive-svg-container")
+    .append("svg")
+    .attr("viewBox", "0 0 1200 1600")
+    .style("border", "1px solid black");
+
+ d3.csv("./data/data.csv", d => {
+    console.log(d);
+});
