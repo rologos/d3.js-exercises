150acaf6aba939504ac7b649d44acf6d3dd6f26c
rologos <170283129+rologos@users.noreply.github.com>
2024-10-26 16:20:59 -0500

    Finalize graph
    Add labels and counts

--- ch03/js/main.js
+++ ch03/js/main.js
@@ -25,19 +25,43 @@ const createViz = (data) => {
         .paddingInner(0.2);
 
     const barAndLabel = svg
-    .selectAll("g")
-    .data(data)
-    .join("g")
-    .attr("transform", d => `translate(0, ${yScale(d.technology)})`);
+        .selectAll("g")
+        .data(data)
+        .join("g")
+        .attr("transform", d => `translate(0, ${yScale(d.technology)})`);
 
     barAndLabel.append("rect")
-    .attr("class", d => {
-        console.log(d);
-        return `bar bar-${d.technology}`;
-    })
-    .attr("width", d => xScale(d.count))
-    .attr("height", yScale.bandwidth())
-    .attr("x", 100)
-    .attr("y", 0)
-    .attr("fill", d => d.technology == "D3.js" ? "yellowgreen" : "skyblue")
+        .attr("class", d => {
+            console.log(d);
+            return `bar bar-${d.technology}`;
+        })
+        .attr("width", d => xScale(d.count))
+        .attr("height", yScale.bandwidth())
+        .attr("x", 120)
+        .attr("y", 0)
+        .attr("fill", d => d.technology == "D3.js" ? "yellowgreen" : "skyblue")
+
+    barAndLabel.append("text")
+        .text(d => d.technology)
+        .attr("x", 112)
+        .attr("y", 12)
+        .attr("text-anchor","end")
+        .style("font-family", "sans-serif")
+        .style("font-size", "11px");
+
+    barAndLabel
+        .append("text")
+        .text(d => d.count)
+        .attr("x", d => 120 + xScale(d.count) + 4)
+        .attr("y", 12)
+        .style("font-family", "sans-serif")
+        .style("font-size", "9px");
+
+    svg
+        .append("line")
+          .attr("x1", 120)
+          .attr("y1", 0)
+          .attr("x2", 120)
+          .attr("y2", 700)
+          .attr("stroke", "black");
 };  
