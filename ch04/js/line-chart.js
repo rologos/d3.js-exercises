af87435ca5747d716aa8a56c5a4e0a8384b82ec1
rologos <170283129+rologos@users.noreply.github.com>
2024-10-29 12:29:13 -0500

    add x axis

--- ch04/js/line-chart.js
+++ ch04/js/line-chart.js
@@ -2,7 +2,7 @@
 d3.csv("./data/daily_precipitations.csv", d3.autoType)
     .then(d => {
         console.log("temperature data",d);
-        drawLineChart(data);
+        drawLineChart(d);
     })
 // Create the line chart here
 const drawLineChart = (data) => {
@@ -11,6 +11,16 @@ const drawLineChart = (data) => {
     const height = 500;
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
+    const firstDate = d3.min(data, d => d.date)
+    const lastDate = d3.max(data, d => d.date)
+    const maxTemp = d3.max(data, d => d.min_temp_F)
+    const xScale = d3.scaleTime()
+        .domain([firstDate,lastDate])
+        .range([0,innerWidth]);
+
+    const yScale = d3.scaleLinear()
+        .domain([0, maxTemp])
+        .range([innerHeight, 0]);
 
     const svg = d3.select("#line-chart")
         .append("svg")
@@ -19,4 +29,9 @@ const drawLineChart = (data) => {
     const innerChart = svg
         .append("g")
         .attr("transform",`translate( ${margin.right}, ${margin.top})`);
+
+    const bottomAxis = d3.axisBottom(xScale);
+        innerChart.append("g")
+            .attr("class","axis-x")
+            .call(bottomAxis)
 };      
