87499af31fe979a5c10f5b7620237113dd85f5e2
rologos <170283129+rologos@users.noreply.github.com>
2024-10-29 17:09:18 -0500

    add line graph

--- ch04/js/line-chart.js
+++ ch04/js/line-chart.js
@@ -1,12 +1,12 @@
 // Load the data here
 d3.csv("./data/weekly_temperature.csv", d3.autoType)
     .then(d => {
-        console.log("temperature data",d);
+        console.log("temperature data", d);
         drawLineChart(d);
     })
 // Create the line chart here
 const drawLineChart = (data) => {
-    const margin = {top: 40, right:170, bottom: 25, left: 40};
+    const margin = { top: 40, right: 170, bottom: 25, left: 40 };
     const width = 1000;
     const height = 500;
     const innerWidth = width - margin.left - margin.right;
@@ -14,9 +14,11 @@ const drawLineChart = (data) => {
     const firstDate = new Date(2021, 0, 1, 0, 0, 0);
     const lastDate = d3.max(data, d => d.date);
     const maxTemp = d3.max(data, d => d.max_temp_F);
+    const aubergine = "#75485E";
+
     const xScale = d3.scaleTime()
-        .domain([firstDate,lastDate])
-        .range([0,innerWidth]);
+        .domain([firstDate, lastDate])
+        .range([0, innerWidth]);
 
     const yScale = d3.scaleLinear()
         .domain([0, maxTemp])
@@ -24,18 +26,18 @@ const drawLineChart = (data) => {
 
     const svg = d3.select("#line-chart")
         .append("svg")
-        .attr("viewBox",`0 0 ${width} ${height}`);
+        .attr("viewBox", `0 0 ${width} ${height}`);
 
     const innerChart = svg
         .append("g")
-        .attr("transform",`translate( ${margin.right}, ${margin.top})`);
+        .attr("transform", `translate( ${margin.right}, ${margin.top})`);
 
     const bottomAxis = d3.axisBottom(xScale)
-            .tickFormat(d3.timeFormat("%b"));
+        .tickFormat(d3.timeFormat("%b"));
 
     innerChart
         .append("g")
-        .attr("class","axis-x")
+        .attr("class", "axis-x")
         .attr("transform", `translate(0, ${innerHeight})`)
         .call(bottomAxis);
 
@@ -43,7 +45,7 @@ const drawLineChart = (data) => {
 
     innerChart
         .append("g")
-        .attr("class","axis-y")
+        .attr("class", "axis-y")
         .call(leftAxis);
 
     //format tickers on x axis
@@ -53,13 +55,42 @@ const drawLineChart = (data) => {
             const nextMonth = new Date(2021, currentMonth.getMonth() + 1, 1);
             return (xScale(nextMonth) - xScale(currentMonth)) / 2;
         })
-        .attr("y","10px")
+        .attr("y", "10px")
 
     //format y axis
     d3.selectAll(".axis-y text")
-        .attr("x","-10px")
+        .attr("x", "-10px")
 
     d3.selectAll(".axis-x text, .axis-y text")
         .style("font-family", "Roboto, sans-serif")
         .style("font-size", "14px");
+
+
+    //add label
+    svg
+        .append("text")
+        .text("Temperature (°F)")
+        .attr("y", 20)
+
+    innerChart
+        .selectAll("circle")
+        .data(data)
+        .join("circle")
+            .attr("r",4)
+            .attr("cx", d => xScale(d.date))
+            .attr("cy", d => yScale(d.avg_temp_F))
+            .attr("fill",aubergine);
+
+    const curveGenerator = d3.line()
+        .x(d => xScale(d.date))
+        .y(d => yScale(d.avg_temp_F))
+        .curve(d3.curveCatmullRom)
+
+    innerChart
+        .append("path")
+        .attr("d",curveGenerator(data))
+        .attr("fill","none")
+        .attr("stroke",aubergine);
+
+    c
 };      
