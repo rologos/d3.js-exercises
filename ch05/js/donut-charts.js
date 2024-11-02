00dafdb34e2035e54c5f61d9aea6b2a0c86367e3
rologos <170283129+rologos@users.noreply.github.com>
2024-11-02 07:32:57 -0500

    add containers for donut charts

--- ch05/js/donut-charts.js
+++ ch05/js/donut-charts.js
@@ -1,5 +1,24 @@
 const drawDonutCharts = (data) => {
   // Generate the donut charts here
 
-  
+  const svg = d3.select("#donut")
+    .append("svg")
+      .attr("viewBox", ` 0 0 ${width} ${height}`)
+
+  const donutContainers = svg
+    .append("g")
+    .attr("transorm", `translate(${margin.left},
+        ${margin.top})`);
+
+    /*********************************************/
+    /*              add area                     */
+    /*********************************************/
+
+  const years = [1975, 1995, 2013];
+  years.forEach( year => {
+    const donutContainer = donutContainers
+      .append("g")
+        .attr("transform", `translate( ${xScale(year)}, ${innerHeight/2})`)
+  })
+
 };
