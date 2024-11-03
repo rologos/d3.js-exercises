5196de805c611a0010536cef23406b524efc5d1f
rologos <170283129+rologos@users.noreply.github.com>
2024-11-03 10:56:28 -0600

    add labels to pie chart

--- ch05/js/donut-charts.js
+++ ch05/js/donut-charts.js
@@ -51,9 +51,40 @@ const drawDonutCharts = (data) => {
   const arcs = donutContainer
     .selectAll(`.arc-${year}`)
     .data(annotatedData)
-    .join("path")
-      .attr("class",`.arc-${year}`)
+    .join("g")
+      .attr("class",`.arc-${year}`);
+
+  arcs  
+    .append("path")
       .attr("d", arcGenerator)
       .attr("fill", d => colorScale(d.data.format));    
+
+    /*********************************************/
+    /*              add labels            */
+    /*********************************************/
+
+  arcs
+    .append("text")
+      .text( d => {
+        d["percentage"] = ( d.endAngle - d.startAngle)
+          / ( 2 * Math.PI);
+        return d3.format(".0%")(d.percentage);
+      })
+      .attr("x", d => {
+        d["centroid"] = arcGenerator
+          .startAngle(d.startAngle)
+          .endAngle(d.endAngle)
+          .centroid();
+        return d.centroid[0];
+      })
+      .attr("y", d => d.centroid[1])
+      .attr("text-anchor", "middle")
+      .attr("dominant-baseline", "middle")
+      .attr("fill", "#f6fafc")
+      .attr("fill-opacity", d => d.percentage
+        < 0.05 ? 0 : 1)
+      .style("font-size", "16px")
+      .style("font-weight", 500);
   });
+
 };
